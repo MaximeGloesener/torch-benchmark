938d4a67b7c5ed94243be425193d26666cd7394a
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-11-01 10:19:24 +0100

    fix(typo): URL to the project

--- setup.py
+++ setup.py
@@ -18,5 +18,5 @@ setup(
     description="torch benchmarking tool",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/MaximeGloesener/torch_benchmark",
+    url="https://github.com/MaximeGloesener/torch-benchmark",
 )
