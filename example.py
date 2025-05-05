997c9d19dd25dcef58bed8bad25f032cdf6abd28
Maxime Gloesener <max.gleu@gmail.com>
2025-05-05 16:56:55 +0200

    update example

--- example.py
+++ example.py
@@ -1,22 +1,12 @@
 import torch
-from torchvision.models import resnet50, ResNet50_Weights
 from pytorch_bench import benchmark
 
 # Load model and example input
-model = resnet50(weights=ResNet50_Weights.DEFAULT)
+model = resnet50(weights="DEFAULT")
 example_input = torch.randn(1, 3, 224, 224)
 
 # Run benchmark
+results = benchmark(model, example_input)
 
-import time
-
-start = time.perf_counter()
-results = benchmark(model, example_input, gpu_only=True)
-end = time.perf_counter()
-
-print("time = ", end-start)
-# Access specific results
-#print(f"GPU FPS: {results['fps_gpu']:.2f}")
-#print(f"CPU FPS: {results['fps_cpu']:.2f}")
-#print(f"Model size: {results['model_size'] / 1024 / 1024:.2f} MB")
-
+print("Benchmark Results:")
+print(results)
